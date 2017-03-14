b52e381fbcea505cf5f38aa305ad1253ce5b0f98
Nicholas Chapman <nick@indigorenderer.com>
2017-03-14 14:32:56 +0000

    Changing back to LLVM 3.4 as 3.6 crashes on Mac.

--- wnt_LLVMVersion.h
+++ wnt_LLVMVersion.h
@@ -7,4 +7,7 @@ Copyright Glare Technologies Limited 2015 -
 
 
 // LLVM version 3.6 should be '36' etc..
-#define TARGET_LLVM_VERSION 36
+#define TARGET_LLVM_VERSION 34
+
+// LLVM 3.6 is the last LLVM version that builds with vs2012.
+// NOTE: llvm 3.6 crashes on Mac.
