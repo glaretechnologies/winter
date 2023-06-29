9099efa22e583582ac56409f0761f81a3dd4873f
Nicholas Chapman <nick@indigorenderer.com>
2023-06-29 23:01:01 +1200

    Made BaseException inherit from glare::Exception.

--- BaseException.h
+++ BaseException.h
@@ -7,21 +7,19 @@ Copyright Glare Technologies Limited 2019 -
 
 
 #include "wnt_Diagnostics.h"
+#include <utils/Exception.h>
 
 
 namespace Winter
 {
 
 
-class BaseException
+class BaseException : public glare::Exception
 {
 public:
-	BaseException(const std::string& s_) : s(s_) {}
+	BaseException(const std::string& s) : glare::Exception(s) {}
 	virtual ~BaseException() {}
-	virtual std::string what() const { return s; } // Get exception message with source position info.
-	virtual std::string messageWithPosition() { return s; } // Exception message, with source position info if there is some.
-private:
-	std::string s;
+	virtual std::string messageWithPosition() { return what(); } // Exception message, with source position info if there is some.
 };
 
 
